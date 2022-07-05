c94e5708f47a25944500841d032e3e371e15159d
GPR <58727992+go-pro-v@users.noreply.github.com>
2022-07-05 15:33:23 +0200

    improve ShowerO'Clock

--- examples/ShowerOClock/ShowerOClock.ino
+++ examples/ShowerOClock/ShowerOClock.ino
@@ -3,6 +3,7 @@
 #define FAST_LED_MODE
 //#define NEOPIXEL_BUS_MODE
 //#define NEOPIXEL_ADAFRUIT_MODE
+#define RING_DURATION 15
 
 #include <RTNTPClient.h>
 #include <RTWifi.h>
@@ -40,17 +41,27 @@ RTButton button(PIN_BUTTON, HIGH, 100);
 
 RTNTPClient ntp(NTP_SERVER);
 
-RTRamp rampShower(100, 255);
+RTRamp rampShower(200, PWMRANGE);
 RTRamp rampLight(1000, 255);
 
 TM1637Display display_7_4(PIN_I2C_CLK, PIN_I2C_DIO);
 
-bool isRingAllowed = false;
+enum class RingState{
+  waiting,
+  ringing,
+  stopped,
+  disabled,
+  ringtest
+};
+/*bool isRingAllowed = false;
+bool isRinging = false;
+bool isRingTest = false;*/
+RingState ring_state = RingState::waiting;
 int ringHour = 0;
 int ringMinute = 0;
-bool isRinging = false;
-bool isRingTest = false;
 bool ledState = LOW;
+bool visualMMI = false;
+bool visualMMI2 = false;
 
 uint8_t led_R = 0;
 uint8_t led_G = 0;
@@ -91,14 +102,15 @@ void setup() {
   wifi.start(
     [](){
       static RTTimer stepTimer(500000);
-      if(stepTimer.isOver())
+      stepTimer.run();
+      if(stepTimer.isTrigged())
       {
-        stepTimer.sync_reset();
         stepChenille();
       }
-    }
+    },
+    [](){visualMMI2 = true;}
   );
-  ntp.setOffset(2*60*60);
+  //ntp.setOffset(2*60*60);
   ntp.start();
   
   rampShower.start();
@@ -107,15 +119,15 @@ void setup() {
   rampLight.set(0);
 
   mqtt.start();
-  mqtt.subscribe("clock/ring", [](String message){isRingAllowed = (message == "1");});
-  mqtt.subscribe("clock/test/ring", [](String message){isRingTest = (message == "1");});
-  mqtt.subscribe("clock/hour", [](String message){ringHour = message.toInt();});
-  mqtt.subscribe("clock/minute", [](String message){ringMinute = message.toInt();});
+  mqtt.subscribe("clock/ring", [](String message){message == "1" ? enableRinging() : disableRinging(); visualMMI = true;});
+  mqtt.subscribe("clock/test/ring", [](String message){message == "1" ? startRingtest() : stopRingtest(); visualMMI = true;});
+  mqtt.subscribe("clock/hour", [](String message){ringHour = message.toInt(); visualMMI = true;});
+  mqtt.subscribe("clock/minute", [](String message){ringMinute = message.toInt(); visualMMI = true;});
   mqtt.subscribe("clock/led", manageLedMqtt);
-  mqtt.subscribe("clock/offset", [](String message){ntp.setOffset(message.toInt()*60);});
-  mqtt.subscribe("clock/led/r", [](String message){led_R = message.toInt();updateLeds();});
-  mqtt.subscribe("clock/led/g", [](String message){led_G = message.toInt();updateLeds();});
-  mqtt.subscribe("clock/led/b", [](String message){led_B = message.toInt();updateLeds();});
+  mqtt.subscribe("clock/offset", [](String message){ntp.setOffset(message.toInt()*60); visualMMI = true;});
+  mqtt.subscribe("clock/led/r", [](String message){led_R = message.toInt();updateLeds(); visualMMI = true;});
+  mqtt.subscribe("clock/led/g", [](String message){led_G = message.toInt();updateLeds(); visualMMI = true;});
+  mqtt.subscribe("clock/led/b", [](String message){led_B = message.toInt();updateLeds(); visualMMI = true;});
   mqtt.subscribe("clock/brightness", manageClockBrightness);
   
   
@@ -134,13 +146,15 @@ void loop() {
   manageDilplayTime();
   manageRinging();
 
-  if(isRinging)
+  switch(ring_state)
   {
-    rampShower.set(255);
-  }
-  else
-  {
-    rampShower.set(0);
+    case RingState::ringing:
+    case RingState::ringtest:
+      rampShower.set(PWMRANGE);
+      break;
+    default:
+      rampShower.set(0);
+      break;
   }
   
   if(rampShower.changed())
@@ -177,9 +191,9 @@ void loop() {
   }
   
   static RTTimer ledTimer(40000);
-  if(ledTimer.isOver())
+  ledTimer.run();
+  if(ledTimer.isTrigged())
   {
-    ledTimer.async_reset();
 #if defined(FAST_LED_MODE)
     FastLED.setBrightness(rampLight.get());
     //noInterrupts();
@@ -237,6 +251,7 @@ void manageClockBrightness(String message)
   if(brightness == 0)
   {
     displayAllowed = false;
+    display_7_4.setBrightness(0);
     uint8_t data[] = {0,0,0,0};
     display_7_4.setSegments(data);
   }
@@ -245,11 +260,56 @@ void manageClockBrightness(String message)
     displayAllowed = true;
     display_7_4.setBrightness(brightness - 1);
   }
+   visualMMI = true;
 }
 
 void manageDilplayTime()
 {
-  if(displayAllowed && ntp.changed())
+  static RTTimer visualMMITimer(500000);
+  static bool isGlitching = false;
+  visualMMITimer.update();
+  if(visualMMI || visualMMI2)
+  {
+    isGlitching = true;
+    visualMMITimer.async_reset();
+    uint8_t data[4] = {0,0,0,0};
+    if(visualMMI)
+    {
+      data[0] = SEG_A|SEG_D|SEG_E|SEG_F;
+      data[1] = SEG_A|SEG_D;
+      data[2] = SEG_A|SEG_D; 
+      data[3] = SEG_A|SEG_B|SEG_C|SEG_D;
+    }
+    else if(visualMMI2)
+    {
+      data[0] = SEG_B|SEG_C|SEG_E|SEG_F;
+      data[1] = SEG_B|SEG_C|SEG_E|SEG_F;
+      data[2] = SEG_B|SEG_C|SEG_E|SEG_F;
+      data[3] = SEG_B|SEG_C|SEG_E|SEG_F;
+    }
+    display_7_4.setSegments(data);
+    visualMMI = false;
+    visualMMI2 = false;
+  }
+  if(isGlitching)
+  {
+    if(visualMMITimer.isOver())
+    {
+      isGlitching = false;
+      if(displayAllowed)
+      {
+        display_7_4.showNumberDecEx(
+          ntp.hour() * 100 + ntp.minute(), 
+          true, true, 4, 0);
+      }
+      else
+      {
+        uint8_t data[] = {0,0,0,0};
+        display_7_4.setSegments(data);
+      }
+    }
+  }
+  else if(displayAllowed && ntp.changed())
   {
     static uint8_t dot = 0;
     dot = ~dot;
@@ -270,47 +330,67 @@ void manageLedMqtt(String message)
     ledState = LOW; 
   }
   updateLeds();
+  visualMMI = true;
 }
-
+  
+void disableRinging()
+{
+  if(ring_state != RingState::disabled)
+  {
+    Serial.println("Disable ring");
+    ring_state = RingState::disabled;
+  }
+}
+void enableRinging()
+{
+  if(ring_state == RingState::disabled)
+  {
+    Serial.println("Enable ring");
+    ring_state = RingState::waiting;
+  }
+}
+void startRingtest()
+{
+  if(ring_state != RingState::disabled && ring_state != RingState::ringtest)
+  {
+    Serial.println("Test ring");
+    ring_state = RingState::ringtest;
+  }
+}
+void stopRingtest()
+{
+  if(ring_state == RingState::ringtest)
+  {
+    Serial.println("Stop test ring");
+    ring_state = RingState::waiting;
+  }
+}
 void manageRinging()
 {
-  if(isRinging && button.isTrigged())
+  if(button.isTrigged())
   {
-    isRinging = false;
-    isRingTest = false;
-    Serial.println("Stop sonne");
-    if(isRingTest)
+    if(ring_state==RingState::ringtest)
     {
       mqtt.publish("clock/test/ring", "0");
     }
-  }
-  
-  if(isRingTest)
-  {
-    isRinging = true;
-  }
-  else if(isRingAllowed)
-  {
-    if(ntp.changed())
+    else if(ring_state==RingState::ringing)
     {
-      if(!isRinging && ntp.hour() == ringHour && ntp.minute() == ringMinute && ntp.second() == 0)
-      {
-        Serial.println("Sonne !");
-        isRinging = true;
-      }
-      else if (isRinging)
-      {
-        if(ntp.hour() != ringHour || ntp.minute() != ringMinute || ntp.second() >= 30)
-        {
-          isRinging = false;
-          Serial.println("Stop sonne");
-        }
-      }
+      ring_state=RingState::stopped;
+      Serial.println("Stop ring");
     }
   }
-  else if(isRinging)
+  if(ring_state==RingState::waiting && ntp.changed() && ntp.hour() == ringHour && ntp.minute() == ringMinute && ntp.second() < RING_DURATION)
   {
-    isRinging = false;
-    Serial.println("Stop sonne");
+    ring_state=RingState::ringing;
+    Serial.println("Ring");
+  }
+  if(
+    (ring_state==RingState::stopped || ring_state==RingState::ringing)
+    && ntp.changed() 
+    && (ntp.hour() != ringHour || ntp.minute() != ringMinute || ntp.second() >= RING_DURATION)
+  )
+  {
+    ring_state=RingState::waiting;
+    Serial.println("Reinit ring");
   }
 }
